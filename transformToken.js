975044aad676df6750332716881271d520dcfd8c
hovelopin <kimhojin3714@naver.com>
2024-04-24 18:47:46 +0900

    registerFormat 적용 완료

--- transformToken.js
+++ transformToken.js
@@ -1,15 +1,30 @@
-const StyleDictionary = require('style-dictionary').extend({
-  source: ['./tokens.json'],
+const StyleDictionary = require('style-dictionary');
+
+// StyleDictionary.registerFormat({
+//   name: 'scss/variables', // 참조할 공간 ( config에 있는 format )
+//   formatter: ({dictionary, platform, options, file}) => {
+//     console.log("dictionary" , dictionary);
+//     console.log("platform" , platform);
+//     console.log("options" , options);
+//     console.log("file" , file);
+//     return JSON.stringify(dictionary.tokens, null, 2);
+//   }
+// });
+
+const styleDictionaryConfig = {
+  source: ['./tokens.json'], // 참조할 token 공간
   platforms: {
-    css: {
-      transformGroup: 'scss',
-      buildPath: 'build/',
+    scss: {
+      transformGroup: 'scss', // 어떤 css를 만들건지
+      buildPath: 'build/', // 만들어지는 공간
       files: [{
         destination: 'variables.scss',
-        format: 'scss/variables'
+        format: 'scss/variables' // registerFormat에 들어갈 name 속성
       }]
     }
   }
-});
+};
 
-StyleDictionary.buildAllPlatforms();
+const StyleDictionaryExtended = StyleDictionary.extend(styleDictionaryConfig);
+
+StyleDictionaryExtended.buildAllPlatforms();
